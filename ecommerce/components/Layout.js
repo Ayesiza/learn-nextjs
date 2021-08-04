2ac3901b05b95bc93e7c035acffb1f589b8ca33a
Ayesiza <yesiha2001@gmail.com>
2021-08-04 05:25:19 +0300

    Nextjs project setup

---
+++ ecommerce/components/Layout.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import Head from 'next/head';
+
+import { AppBar, Container, Toolbar, Typography } from '@material-ui/core';
+import useStyles from "../utils/styles";
+
+
+
+
+export default function Layout({children}) {
+    const classes = useStyles();
+    return (
+        <div>
+            <Head>
+                <title>Next App</title>
+            </Head>
+            <AppBar position ="static" className={classes.navbar}>
+                <Toolbar>
+                    <Typography>Ecommerce</Typography>
+                </Toolbar>
+
+            </AppBar>
+            <Container className={classes.main}>
+                {children}
+            </Container>
+            <footer className={classes.footer}>
+                <Typography>
+                    All rights reserved to Ecommerce.
+                </Typography>
+            </footer>
+        </div>
+    )
+}
