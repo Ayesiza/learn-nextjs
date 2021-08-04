35fb308ba721771c893cfc78afdc099a6f18f254
Ayesiza <yesiha2001@gmail.com>
2021-08-04 06:40:30 +0300

    create a list of products

--- ecommerce/pages/index.js
+++ ecommerce/pages/index.js
@@ -1,21 +1,44 @@
-import Head from 'next/head'
-import Image from 'next/image'
-import Layout from '../components/Layout'
-import styles from '../styles/Home.module.css'
+import {
+  Grid,
+  Card,
+  CardActionArea,
+  CardMedia,
+  CardContent,
+  CardActions,
+  Typography,
+  Button,
+} from "@material-ui/core";
+import Layout from "../components/Layout";
+import data from "../utils/data";
 
 export default function Home() {
   return (
     <Layout>
-  <div>
-      <h1>Products</h1>
-      <ul>
-        <li>Product1</li>
-        <li>Product2</li>
-        <li>Product3</li>
-      </ul>
-    </div>
+      <div>
+        <h1>Products</h1>
+        <Grid container spacing={3}>
+          {data.products.map((product) => (
+            <Grid item md={4} key={product.name}>
+              <Card>
+                <CardActionArea>
+                  <CardMedia
+                    component="img"
+                    image={product.image}
+                    title={product.name}
+                  ></CardMedia>
+                  <CardContent>
+                    <Typography>{product.name}</Typography>
+                  </CardContent>
+                </CardActionArea>
+                <CardActions>
+                  <Typography>${product.price}</Typography>
+                  <Button size="small" color="primary">Add to Cart</Button>
+                </CardActions>
+              </Card>
+            </Grid>
+          ))}
+        </Grid>
+      </div>
     </Layout>
-    
-   
-  )
+  );
 }
